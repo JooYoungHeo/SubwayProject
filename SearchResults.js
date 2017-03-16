234cfdb443113d5a1958b854fd472e5d74b75662
JooYoungHeo <jyhur900226@naver.com>
2017-03-16 11:33:18 +0900

    [작업] 결과 페이지 탭 => 상세페이지 이동

--- SearchResults.js
+++ SearchResults.js
@@ -8,6 +8,8 @@ import {
   Text
 } from 'react-native';
 
+import PropertyView from './PropertyView';
+
 const styles = StyleSheet.create({
   thumb: {
     width: 80,
@@ -47,6 +49,12 @@ class SearchResults extends Component{
 
   rowPressed(listerURL){
     var property = this.props.listings.filter(prop => prop.lister_url === listerURL)[0];
+
+    this.props.navigator.push({
+      title: "Property",
+      component: PropertyView,
+      passProps: {property: property}
+    });
   }
 
   renderRow(rowData, sectionId, rowId){
